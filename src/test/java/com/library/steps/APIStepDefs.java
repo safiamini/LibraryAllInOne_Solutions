c457cf78e01a6a289a81a82a460ae34e2e659330
mehmetCt <mehmet@cybertekschool.com>
2023-12-20 13:52:58 -0500

    US03 is started

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -48,6 +48,7 @@ public class APIStepDefs {
     public void status_code_should_be(Integer statusCode) {
         thenPart.statusCode(statusCode);
     }
+
     @Then("Response Content type is {string}")
     public void response_content_type_is(String contentType) {
         thenPart.contentType(contentType);
@@ -94,6 +95,34 @@ public class APIStepDefs {
         }
 
     }
+
+    /**
+     * US 03 RELATED
+     */
+
+
+    @Given("Request Content Type header is {string}")
+    public void request_content_type_header_is(String contentType) {
+
+    }
+    @Given("I create a random {string} as request body")
+    public void i_create_a_random_as_request_body(String randomData) {
+
+
+    }
+    @When("I send POST request to {string} endpoint")
+    public void i_send_post_request_to_endpoint(String endpoint) {
+
+    }
+    @Then("the field value for {string} path should be equal to {string}")
+    public void the_field_value_for_path_should_be_equal_to(String path, String value) {
+
+    }
+    @Then("{string} field should not be null")
+    public void field_should_not_be_null(String path) {
+
+    }
+
 
 
 }
