fb0b87a5f05d67cf1d2085e40bb74fd1967879b8
mehmetCt <mehmet@cybertekschool.com>
2023-12-20 14:49:24 -0500

    US03 SC 2 is started

--- src/test/java/com/library/steps/UIStepDefs.java
+++ src/test/java/com/library/steps/UIStepDefs.java
@@ -1,15 +1,26 @@
 package com.library.steps;
 
+import com.library.pages.BookPage;
+import com.library.pages.LoginPage;
+import com.library.utility.BrowserUtil;
 import io.cucumber.java.en.Given;
 
 public class UIStepDefs {
 
-    @Given("I logged in Library UI as {string}")
-    public void i_logged_in_library_ui_as(String string) {
+    LoginPage loginPage=new LoginPage();
 
+    BookPage bookPage=new BookPage();
+
+    @Given("I logged in Library UI as {string}")
+    public void i_logged_in_library_ui_as(String userType) {
+        loginPage.login(userType);
+        BrowserUtil.waitFor(3);
     }
     @Given("I navigate to {string} page")
-    public void i_navigate_to_page(String string) {
+    public void i_navigate_to_page(String moduleName) {
+
+        bookPage.navigateModule(moduleName);
+        BrowserUtil.waitFor(3);
 
     }
 }
