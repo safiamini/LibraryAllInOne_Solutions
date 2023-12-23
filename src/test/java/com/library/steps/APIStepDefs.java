8693a414b5d1db6876350814f8d34f282eb25372
mehmetCt <mehmet@cybertekschool.com>
2023-12-23 11:51:18 -0500

    US04 is DONE

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -316,5 +316,17 @@ public class APIStepDefs {
 
     }
 
+    /**
+     * US 05
+     */
+    @Given("I logged Library api with credentials {string} and {string}")
+    public void i_logged_library_api_with_credentials_and(String email, String password) {
+
+    }
+    @Given("I send token information as request body")
+    public void i_send_token_information_as_request_body() {
+
+    }
+
 
 }
