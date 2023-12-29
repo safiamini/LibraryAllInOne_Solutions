1cefa2a83d0ba18b73b8f3d60ad591cbd51d059e
safiamini <134248585+safiamini@users.noreply.github.com>
2023-12-28 19:00:05 -0600

    Update CukesRunner.java

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,8 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
-        dryRun = false,
-        tags = "@us05"
+            publish = true
 )
 public class CukesRunner {
 
