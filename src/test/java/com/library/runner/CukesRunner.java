74b69d589ea470983ded69caabb624e28ac36b01
Safi_Amin <Safi.amini786@gmail.com>
2023-12-30 17:55:21 -0600

    updated ip address

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
-        dryRun = false,
-        tags = "@us05"
+        publish = true
+
 )
 public class CukesRunner {
 
